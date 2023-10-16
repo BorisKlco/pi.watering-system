a50cc350e80925092bbd2d655dfbc460f1bd1292
BorisKlco <klco@pm.me>
2023-10-16 12:45:37 +0200

    Create controller.py

---
+++ controller.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as GPIO
+
+# GPIO/BCM Number
+GPIO.setmode(GPIO.BCM)
+
+# Physical/Board pin
+# GPIO.setmode(GPIO.BOARD)
+
+
+class Relay:
+    def __init__(self, pin):
+        self.pin = pin
+
+    def init(self):
+        GPIO.setup(self.pin, GPIO.OUT)
+
+    def on(self):
+        GPIO.output(self.pin, GPIO.LOW)
+
+    def off(self):
+        GPIO.output(self.pin, GPIO.HIGH)
+
+
+class Sensor:
+    def __init__(self, pin):
+        self.pin = pin
+
+    def init(self):
+        GPIO.setup(self.pin, GPIO.IN)
+
+    def data(self):
+        return GPIO.input(self.pin)
