f33569183b93279c0c66140db986f8196e9e7d85
BorisKlco <klco@pm.me>
2023-10-17 10:57:27 +0200

    intervals

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
 import time
+import math
 
 last_watering = time.time()
+SLEEP_INTERVAL = 1  # 1 hour - 3600
+WATERING_INTERVAL = 6  # 12 hour - 43200
 
 while True:
-    time.sleep(1) # 1 hour - 3600
-    watering_diff = time.time() - last_watering
-    dry_soil = False # sensor_output()
-    print(watering_diff)
+    time.sleep(SLEEP_INTERVAL)
+    watering_diff = math.floor(time.time() - last_watering)
+    water_now = True if watering_diff > WATERING_INTERVAL else False
+    dry_soil = False  # sensor_output()
+    print("INFO: ", last_watering, water_now)
+    if dry_soil or water_now:
+        print("WATERING!!!")
+        last_watering = time.time()
