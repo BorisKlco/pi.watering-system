6984d71b703fdda45a1d3d883d556be204294850
BorisKlco <klco@pm.me>
2023-10-18 14:33:30 +0200

    htmx

--- server/server.py
+++ server/server.py
@@ -18,22 +18,35 @@ db.init_app(app)
 class Records(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.String)
+    path = db.Column(db.String)
     image = db.Column(db.String)
     water = db.Column(db.Integer, default=50)
 
     def __repr__(self):
-        return f"-\nID:{self.id}\nTIME:{self.time}\nPHOTO:{self.image}\n-"
+        return f"-\nID:{self.id}\nTIME:{self.time}\nPHOTO:{self.image}\nPATH:{self.path}\n-"
 
 
 with app.app_context():
     db.create_all()
 
 
-@app.route("/q", methods=["GET"])
-def get_records():
-    records = Records.query.order_by(Records.id.desc()).all()
-    count = Records.query.count()
-    return render_template("index.html", records=records, count=count)
+@app.route("/", methods=["GET"])
+def main_page():
+    return render_template("index.html")
+
+
+@app.route("/watering", methods=["GET"])
+def last_watering():
+    record = Records.query.order_by(Records.id.desc()).first()
+    time_converted = time.strftime(
+        "%d/%m/%y - %H:%M:%S", time.localtime(float(record.time))
+    )
+    return render_template("watering.html", time=time_converted, photo=record.image)
+
+
+@app.route("/history", methods=["GET"])
+def history():
+    return render_template("history.html")
 
 
 @app.route("/store-record", methods=["POST"])
@@ -56,15 +69,15 @@ def index():
         ###### PHOTO
         photo = request.files.get("file")
         if request.form.get("filename") is None:
-            photo_name = time.strftime("%y%m%d%H") + ".jpg"
+            photo_name = time.strftime("%y%m%d-%H") + ".jpg"
         else:
             photo_name = request.form.get("filename")
 
-        path_to_photo = os.path.join(PHOTO_FOLDER, photo_name)  # type: ignore
+        path_to_photo = os.path.join(PHOTO_FOLDER, "static", photo_name)  # type: ignore
         photo.save(path_to_photo)  # type: ignore
 
         ###### SAVE TO DB
-        data_for_db = Records(time=watering_time, image=path_to_photo)
+        data_for_db = Records(time=watering_time, image=photo_name, path=path_to_photo)
         db.session.add(data_for_db)
         db.session.commit()
         ###### SAVE TO DB
