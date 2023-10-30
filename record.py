39b17885927d7d8741544750d02fc11b2a777577
BorisKlco <klco@pm.me>
2023-10-30 10:49:07 +0100

    Create record.py

---
+++ record.py
@@ -0,0 +1,31 @@
+# crontab -e
+# 0 8 * * * python3 record.py
+# 0 12 * * * python3 record.py
+# 0 19 * * * python3 record.py
+
+import os
+import time
+import requests
+from picamera import PiCamera
+
+camera = PiCamera()
+camera.resolution = (1280, 720)
+
+SERVER = "http://127.0.0.1:5173/store-record"
+
+
+def record():
+    try:
+        camera.start_preview()
+        photo_name = time.strftime("%y%m%d%H") + ".jpg"
+        data_photo = os.getcwd() + "/photos/" + photo_name
+        time.sleep(2)
+        camera.capture(data_photo)
+
+        data = {"filename": photo_name, "water": 0}
+
+        files = {"file": (photo_name, open(data_photo, "rb"))}
+        req = requests.post(SERVER, data=data, files=files, timeout=15)
+        print("Sending record -> ", req.text, req.status_code)
+    except:
+        print("Error while sending record...")
